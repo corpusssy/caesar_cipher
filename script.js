a466df5b9df45baa5dd63a4a774bdf593806e509
corpusssy <107024953+corpusssy@users.noreply.github.com>
2022-06-12 01:49:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const WHITESPACE = " ";
 
 let random = Math.floor(Math.random() * (SHIFT.length));
 
+// Create a function that ciphers the user's input
 function cipher() {
+    
+    // Create and initialize a variable that will store in the ciphered version of the user's input
     let new_text = "";
 
+    // Store the user's input inside a variable
     let text = document.getElementById("text").value;
     text = text.toLowerCase();
 
+    // Iterate through the user's input and cipher it
     for (let i = 0; i < text.length; i++) {
         if (text[i].includes(WHITESPACE) == false) {
             let position = ALPHABET.indexOf(text[i]);
@@ -20,20 +25,25 @@ function cipher() {
 
     }
 
+    // Convert the ciphered message into all capital letters
     new_text = new_text.toUpperCase();
 
+    // Output the ciphered version of the user's input
     document.getElementById("output").innerText = `Ciphered text:\n${new_text}`;
-    document.getElementById("instructions").innerText = "";
 
 }
 
+// Create a function the deciphers the user's input
 function decipher() {
 
+    // Create a function that deciphers the user's input
     let new_text = "";
 
+    // Store the user's input inside a variable
     let text = document.getElementById("text").value;
     text = text.toLowerCase();
 
+    // Iterate through the user's input and decipher it
     for (let i = 0; i < text.length; i++) {
         if (text[i].includes(WHITESPACE) == false) {
             let position = ALPHABET.indexOf(text[i]);
@@ -44,8 +54,7 @@ function decipher() {
         }
     }
 
+    // Output the deciphered version of the user's input
     document.getElementById("output").innerText = `Deciphered text:\n${new_text}`;
-    document.getElementById("instructions").innerText = "";
-
 }
     
